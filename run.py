faa577a36dc975bf0335b90c5689c1cc05ca8cc1
ChrisKellFSD <chriskellcsn@gmail.com>
2022-08-14 11:15:17 +0000

    Add extra stages to hangman lives

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from words_for_hangman import hangman_words
 
 
-def hangman_word():
+def get_word():
     word = random.choice(hangman_words)
     return word.upper()
 
@@ -18,14 +18,6 @@ def clear_terminal():
 def main_title():
     print(
         """
-.-. .-.  .--.  .-. .-. .---. .-.   .-.  .--.  .-. .-.
-| {_} | / {} \ |  `| |/   __}|  `.'  | / {} \ |  `| |
-| { } |/  /\  \| |\  |\  {_ }| |\ /| |/  /\  \| |\  |
-`-' `-'`-'  `-'`-' `-' `---' `-' ` `-'`-'  `-'`-' `-'                                                                                                          
-        """
-    )
-    print(
-        """
           +---+
           |   |
           O   |
@@ -35,50 +27,215 @@ def main_title():
         =========
         """
     )
+    print(
+        """
+.-. .-.  .--.  .-. .-. .---. .-.   .-.  .--.  .-. .-.
+| {_} | / {} \ |  `| |/   __}|  `.'  | / {} \ |  `| |
+| { } |/  /\  \| |\  |\  {_ }| |\ /| |/  /\  \| |\  |
+`-' `-'`-'  `-'`-' `-' `---' `-' ` `-'`-'  `-'`-' `-'                                                                                                          
+        """
+    )
 
 
-def play_game(word):
-    chances = 7
-    guesses = []
-    guessed_word = []
-    finished = False
-
-    while not finished:
-        for char in word:
-            if char.lower() in guesses:
-                print(char, end=" ")
-            else:
-                print("_", end=" ")
-        print("")
-       
-        guess = input(f"You have {chances} chances left. : ")
-        guesses.append(guess)
-        if guess.lower() not in word.lower():
-            chances -= 1
-            print("Unlucky! Try again!")
-            if chances == 0:
-                break
-        if ''.join(guessed_word) == word:
-            print("Well done! you have gussed the correct word")
-
-        finished = True
-        for char in word:
-            if char not in guesses:
-                finished = False
-
-    if finished:
-        print(f"Well done! You guessed {word} correctly!")
-    else:
-        print(f"Tough Luck. To ease your pain, it was {word}")
-
-
-def run_game():
+def rules():
     """
-    Runs the game
+    Display rules after the title
     """
+    clear_terminal()
+    print(
+        """
+            Thank you for choosing to play Hangman.
+            You will be presented with a secret word.
+            You must guess a letter each round.
+            If you are correct, you will see the letter.
+            If you are wrong, you will lose a live.
+            You have as many goes as it takes for the man to be hanged.
+            You can choose the difficulty level (Not yet!)
+            \n
+            Good luck and have fun!
+            """
+            )
+
+    input("Press enter to return to the main menu\n")
+
+
+def play(word):
+    hangman_word = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    chances = 6
     main_title()
-    word = hangman_word()
-    play_game(word)
+    print(display_hangman(chances))
+    print(hangman_word)
+    print("\n")
+    while not guessed and chances > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                clear_terminal()
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                clear_terminal()
+                print(guess, "is not in the word.")
+                chances -= 1
+                guessed_letters.append(guess)
+            else:
+                clear_terminal()
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(hangman_word)
+                ind = [i for i, letter in enumerate(word) if letter == guess]
+                for index in ind:
+                    word_as_list[index] = guess
+                hangman_word = "".join(word_as_list)
+                if "_" not in hangman_word:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                clear_terminal()
+                print("You already guessed the word", guess)
+            elif guess != word:
+                clear_terminal()
+                print(guess, "is not the word.")
+                chances -= 1
+                guessed_words.append(guess)
+            else:
+                clear_terminal()
+                guessed = True
+                hangman_word = word
+        else:
+            clear_terminal()
+            print("Not a valid guess.")
+        print(display_hangman(chances))
+        print(hangman_word)
+        print("\n")
+    if guessed:
+        clear_terminal()
+        print("Congrats, you guessed the word! You win!")
+    else:
+        clear_terminal()
+        print("""
+   ____                         ___                 _ 
+  / ___| __ _ _ __ ___   ___   / _ \__   _____ _ __| |
+ | |  _ / _` | '_ ` _ \ / _ \ | | | \ \ / / _ \ '__| |
+ | |_| | (_| | | | | | |  __/ | |_| |\ V /  __/ |  |_|
+  \____|\__,_|_| |_| |_|\___|  \___/  \_/ \___|_|  (_)
+                                                      
+        """)
+        print("\n")
+        print("The word was " + word + ". Maybe next time!")
 
 
-run_game()
+def display_hangman(chances):
+    stages = [  
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -------
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -------
+                """,
+                """
+                   --------
+                   |      
+                   |      
+                   |    
+                   |      
+                   |     
+                   -------
+                """,
+                """
+                   
+                   |      
+                   |      
+                   |    
+                   |      
+                   |     
+                   -------
+                """,
+                """
+                   
+                        
+                         
+                       
+                         
+                       
+                   -------
+                """
+    ]
+    return stages[chances]
+
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+
+if __name__ == "__main__":
+    main()
